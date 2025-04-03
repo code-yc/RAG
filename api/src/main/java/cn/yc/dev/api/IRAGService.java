2e837ee0f8c4179e787f74dfc4ac5a950e7c8c85
YC <code.yc@outlook.com>
2025-04-03 14:17:25 +0800

    feat: Ollama RAG 知识库接口服务实现

---
+++ api/src/main/java/cn/yc/dev/api/IRAGService.java
@@ -0,0 +1,13 @@
+package cn.yc.dev.api;
+
+import cn.yc.dev.api.response.Response;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IRAGService {
+
+    Response<List<String>> queryRagTagList();
+
+    Response<String> uploadFile(String ragTag, List<MultipartFile> files);
+}
