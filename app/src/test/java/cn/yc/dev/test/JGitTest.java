55f1f6bc7560eb61842f34907433463e968be115
YC <code.yc@outlook.com>
2025-04-03 16:04:20 +0800

    feat: 解析Git仓库代码库到知识库

---
+++ app/src/test/java/cn/yc/dev/test/JGitTest.java
@@ -0,0 +1,90 @@
+package cn.yc.dev.test;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.PathResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JGitTest {
+
+    @Resource
+    private OllamaChatClient ollamaChatClient;
+    @Resource
+    private TokenTextSplitter tokenTextSplitter;
+    @Resource
+    private SimpleVectorStore simpleVectorStore;
+    @Resource
+    private PgVectorStore pgVectorStore;
+
+    @Test
+    public void test() throws Exception {
+        String repoURL = "<仓库名称>";
+        String username = "<用户名>";
+        String password = "<验证令牌>";
+
+        String localPath = "./cloned-repo";
+        log.info("克隆路径：" + new File(localPath).getAbsolutePath());
+
+        FileUtils.deleteDirectory(new File(localPath));
+
+        Git git = Git.cloneRepository()
+                .setURI(repoURL)
+                .setDirectory(new File(localPath))
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
+                .call();
+
+        git.close();
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Files.walkFileTree(Paths.get("./cloned-repo"), new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+                try {
+                    log.info("文件路径:{}", file.toString());
+
+                    PathResource resource = new PathResource(file);
+                    TikaDocumentReader reader = new TikaDocumentReader(resource);
+
+                    List<Document> documents = reader.get();
+                    List<Document> documentSplitterList = tokenTextSplitter.apply(documents);
+
+                    documents.forEach(doc -> doc.getMetadata().put("knowledge", "GoWebMini"));
+                    documentSplitterList.forEach(doc -> doc.getMetadata().put("knowledge", "GoWebMini"));
+
+                    pgVectorStore.accept(documentSplitterList);
+
+                } catch (Exception e) {
+                    log.error("上传文件 {} 时发生错误: {}", file, e.getMessage());
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+
+}
